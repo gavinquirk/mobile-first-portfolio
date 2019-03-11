8809133d573665dee8af6f58ecfc3eca54c035a7
Gavin Quirk <gavinquirk@gmail.com>
2019-03-10 17:32:47 -0700

    added badges to landing component

--- src/components/Lander/Lander.js
+++ src/components/Lander/Lander.js
@@ -3,6 +3,12 @@ import { Link, animateScroll as scroll } from 'react-scroll';
 
 import ButtonLarge from '../Common/Buttons/ButtonLarge/ButtonLarge';
 
+import badge_html_css from '../../images/badge_html_css.png';
+import badge_javascript_jquery from '../../images/badge_javascript_jquery.png';
+import badge_node_express from '../../images/badge_node_express.png';
+import badge_react from '../../images/badge_react.png';
+import badge_sql_nosql from '../../images/badge_sql_nosql.png';
+
 import './Lander.css';
 
 class Lander extends Component {
@@ -13,6 +19,19 @@ class Lander extends Component {
           <div className='intro'>
             <h1>Gavin Quirk</h1>
             <h2>Full Stack Web Developer</h2>
+          </div>
+          <div className='certificates'>
+            <img src={badge_html_css} alt='HTML and CSS Certification' />
+            <img
+              src={badge_javascript_jquery}
+              alt='JavaScript and jQuery certification'
+            />
+            <img
+              src={badge_node_express}
+              alt='Node and Express certification'
+            />
+            <img src={badge_react} alt='React certification' />
+            <img src={badge_sql_nosql} alt='SQL and noSQL certification' />
           </div>
         </div>
         <div className='side-b'>
